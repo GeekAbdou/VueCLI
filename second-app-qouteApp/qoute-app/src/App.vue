ae098fd145abc55340741c58e4816cd20ca038f8
Abd-Elrahman Elsayed <abd.hassan98@eng-st.cu.edu.eg>
2021-11-07 02:11:25 +0200

    adding qoute

--- second-app-qouteApp/qoute-app/src/App.vue
+++ second-app-qouteApp/qoute-app/src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="container">
+    <new-qoute @createQoute="createQoute"> </new-qoute>
     <app-qoute-grid :qoutes="qoutes"> </app-qoute-grid>
   </div>
 </template>
 
 <script>
 import QouteGrid from "./components/QouteGrid.vue";
+import NewQoute from "./components/NewQoute.vue";
 
 export default {
   data: function () {
     return {
-      qoutes: ['Just Sinlge Qoute','Just Sinlge Qoute','Just Sinlge Qoute'],
+      qoutes: [],
       maxQoutes: 10,
     };
   },
   components: {
     appQouteGrid: QouteGrid,
+    newQoute: NewQoute,
+  },
+  methods: {
+      createQoute(qoute){
+          this.qoutes.push(qoute);
+      }
   },
 };
 </script>
---
+++ second-app-qouteApp/qoute-app/src/components/NewQoute.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+        <form action="">
+            <textarea class="add-qoute" v-model="qoute"></textarea>
+            <button @click.prevent="createQoute">Add Qoute</button>
+        </form>
+   </div>
+</template> 
+
+<script>
+
+export default { 
+    data: function(){
+        return{
+            qoute:"",
+        }
+    },
+    methods: {
+        createQoute(){
+            this.$emit('createQoute',this.qoute);
+            this.qoute='';
+        }
+    },
+};
+</script>
+
+<style></style>
