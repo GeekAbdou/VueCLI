5402320e15e5942a7ea0c12e017d0eb73b547d62
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-02-06 00:40:27 +0200

    toggle fade

--- Vue-animation/src/views/Home.vue
+++ Vue-animation/src/views/Home.vue
@@ -2,29 +2,51 @@
   <div class="home">
     <Toast v-if="showToast" />
     <Todos @badValue="triggerToast" />
+    <transition name="fade"> 
+      <div v-if="showP">Hello from Paragraph.</div>
+    </transition>
+    <button @click="showP = !showP">toggle</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Toast from '../components/Toast'
-import Todos from '../components/Todos'
+import { ref } from "vue";
+import Toast from "../components/Toast";
+import Todos from "../components/Todos";
 
 export default {
   components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
+    const showP = ref(false);
 
     const triggerToast = () => {
       showToast.value = true;
-      setTimeout(() => showToast.value = false, 3000)
-    }
+      setTimeout(() => (showToast.value = false), 3000);
+    };
 
-    return { showToast, triggerToast }
-  }
-}
+    return { showToast, triggerToast,showP };
+  },
+};
 </script>
 
 <style>
-
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade-enter-active {
+    transition: all 2s ease;
+  }
+  .fade-leave-from {
+    opacity: 1;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-leave-active {
+    transition: all 1s linear;
+  }
 </style>
